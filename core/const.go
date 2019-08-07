384487a9e8249681a632d36f7116cc1d762b36fc
lisen <lisen@youzan.com>
2019-08-07 23:02:41 +0800

    add task to queue and pop task from queue

---
+++ core/const.go
@@ -0,0 +1,3 @@
+package core
+
+const TaskQueueKey = "task_engine_queue"
--- core/model/task_queue.go
+++ core/model/task_queue.go
@@ -1,25 +1,49 @@
 package model
 
 import (
+	"fmt"
 	"github.com/leason00/task-engine/core"
+	"strconv"
+	"strings"
 )
 
-// 任务队列信息
-type TaskQueue struct{
+// 任务队列
+type TaskQueueBase struct{
 	Id int
 	Key string
 }
 
-// 从redis对列弹出一个任务
-func (*TaskQueue) PopTask() (*TaskQueue, error) {
+// 任务队列详细信息
+type TaskQueue struct{
+	TaskQueue
+}
 
+func NewTaskQueue(id int, key string) *TaskQueueBase {
+	return &TaskQueueBase{
+		Id: id,
+		Key: key,
+	}
+}
+
+// 从redis队列弹出一个任务
+func (t *TaskQueueBase) PopTask() (*TaskQueueBase, error) {
+	result := core.Redis.RPop(core.TaskQueueKey).Val()
+	a := strings.Split(result, ":")
+	id, err := strconv.Atoi(a[0])
+	if err != nil {
+		return nil, err
+	}
+	return NewTaskQueue(id, a[0]), nil
 }
 
 // 向redis对列增加一个任务
-func (*TaskQueue) AddTask(id int, key string) (*TaskQueue, error) {
-	//info := &TaskQueue{
-	//	Id:          id,
-	//	Key:      key,
-	//}
-	core.Redis.HSet()
+func (t *TaskQueueBase) AddTask(id int, key string) {
+	task := fmt.Sprintf("%s:%s", strconv.Itoa(id), key)
+	core.Redis.LPush(core.TaskQueueKey, task)
+}
+
+
+// 从mysql获取待执行的任务
+func (t *TaskQueue) GetTasks(count int)  (*TaskQueueBase, error) {
+
 }
